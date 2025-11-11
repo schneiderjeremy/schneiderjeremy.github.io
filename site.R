791eec64e99eadbaec1204f16a5cff16a941fe31
Jeremy Schneider <92173902+schneiderjeremy@users.noreply.github.com>
2025-11-11 14:27:00 -0500

    Update site.R

--- site.R
+++ site.R
@@ -1,2 +1,6 @@
 if (!file.exists(".nojekyll")) file.create(".nojekyll")
+if (!requireNamespace("rmarkdown", quietly = TRUE)) {
+  install.packages("rmarkdown", repos = "https://cloud.r-project.org")
+}
 rmarkdown::render("index.Rmd", output_file = "index.html", output_dir = ".")
+cat("Built index.html. Commit index.html, site_libs/, and .nojekyll.\n")
